b077ba18a705820145f8edcbfc9681aacb0d8c60
V. Bruk <vbruk@hey.com>
2021-10-27 12:40:16 -0300

    Adjustments on Application Runner

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,13 @@ module DevCoin
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.i18n.default_locale = :es
+    config.i18n.available_locales = [:es, :en, :ru]
+    config.i18n.fallbacks = [:en]
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
+    config.autoload_paths += Dir[Rails.root.join("app", "classes", "*/")]
+    config.autoload_paths += Dir[Rails.root.join("app", "models", "*/")]
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -18,5 +25,8 @@ module DevCoin
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # For replacing ActiveStorage unique attachments
+    config.active_storage.replace_on_assign_to_many = true
   end
 end
